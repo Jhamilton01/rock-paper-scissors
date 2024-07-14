94c641c90cf46296b69f7e6b790186c534f1bbfd
Jastine Hamilton <hamiltonchase14@gmail.com>
2024-07-14 18:22:00 -0400

    Step 3 changes created a main function and worked up to step 3 where i removed the console.log and replaced them with DOM

--- javascript.js
+++ javascript.js
@@ -1,80 +1,114 @@
-let humanScore = 0
-let computerScore = 0
+//AUTHOR JESS HAMILTON
 
+function main(){
 
-function getComputerChoice(){
+    let playerScore = 0
+    let cpuScore = 0
+    const rps = document.querySelectorAll("#rps button")
+    const resultsDiv = document.querySelector('#results')
+    
+    rps.forEach((button) => {
+        button.addEventListener('click', () => {
+            let player = button.id
+            let cpu = getComputerChoice()
+            console.log('This is the player ',player)
+            console.log('This is the cpu ',cpu)
 
-let choice = getRandomInt(3)
-if (choice == 0){
-    return "rock"
-} 
-else if (choice == 1){
-    return "paper"
-}
-else {
-    return "scissors"
-}
+            let round = playRound(cpu, player)
+        })
+    })
+
 }
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-function getHumanChoice(){
-    let choice = prompt("Enter r for rock, p for paper, or s for scissors: ")
+function getComputerChoice(){
 
-    if (choice == "r"){
+    let choice = getRandomInt(3)
+    if (choice == 0){
         return "rock"
     } 
-    else if (choice == "p"){
+    else if (choice == 1){
         return "paper"
     }
-    else (choice == "s") 
+    else {
         return "scissors"
+    }
 }
 
 
 function playRound(computer, player){
     if (computer == player) {
-        console.log("Tied!")
+        console.log("tied")
         return "Tied!"
     }
     else if ((computer == "rock" && player == "scissors") ||
             (computer == "paper" && player == "rock") ||
-            (computer == "scissors" && player == "paper")) {
+            (computer == "scissors" && player == "paper")) 
+            {
                 console.log("Computer Wins!")
                 return "Computer Wins!"
-    }
-    else {
+    }else {
         console.log("Player Wins!")
         return "Player Wins!"
     }
 
 }
 
+function displayResults(player, cpu, result, playerScore, cpuScore){
 
-while (humanScore < 5 && computerScore < 5){
-    let computer = getComputerChoice()
-    let player = getHumanChoice()
-    let result = playRound(computer, player)
+    resultsDiv.innerHTML = ''
 
-    if (result == "Player Wins!") {
-        humanScore++
-    } else if (result == "Computer Wins!") {
-        computerScore++
-    }
-    console.log("Your score: ", humanScore)
-    console.log("Computer score: ",computerScore)
+    const playerChoice = document.createElement('p')
+    playerChoice.textContent = 'Player: ${player}'
 
-    alert(`Computer choice: ${computer}\nYour choice: ${player}\nResult: ${result}\nYour score: ${humanScore}\nComputer score: ${computerScore}`);
+    const cpuChoice = document.createElement('p')
+    cpuChoice.textContent = 'CPU: ${cpu}'
+
+    const roundResult = document.createElement('p')
+    roundResult.textContent = 'Results: ${result}'
+
+    const playerScoreDisplay = document.createElement('p')
+    playerScoreDisplay.textContent = 'Player: ${playerScore}'
+
+    const cpuScoreDisplay = document.createElement('p')
+    cpuScoreDisplay.textContent = 'CPU: ${cpuScore}'
+
+    resultsDiv.appendChild(playerChoice)
+    resultsDiv.appendChild(cpuChoice)
+    resultsDiv.appendChild(roundResult)
+    resultsDiv.appendChild(playerScoreDisplay)
+    resultsDiv.appendChild(cpuScoreDisplay)
+
+
 }
 
-if (humanScore === 5){
-    console.log("You win!")
-} else {
-    console.log("You lost")
-}
+main()
 
 
 
 
+    // while (humanScore < 5 && computerScore < 5){
+//     let computer = getComputerChoice()
+//     let player = getHumanChoice()
+//     let result = playRound(computer, player)
+
+//     if (result == "Player Wins!") {
+//         humanScore++
+//     } else if (result == "Computer Wins!") {
+//         computerScore++
+//     }
+//     console.log("Your score: ", humanScore)
+//     console.log("Computer score: ",computerScore)
+
+//     alert(`Computer choice: ${computer}\nYour choice: ${player}\nResult: ${result}\nYour score: ${humanScore}\nComputer score: ${computerScore}`);
+// }
+
+
+// if (humanScore === 5){
+//     console.log("You win!")
+// } else {
+//     console.log("You lost")
+// }
