0086e156ccd46c0d6750bb59faa9e9cd066a3e9e
Jastine Hamilton <hamiltonchase14@gmail.com>
2024-07-18 15:42:18 -0400

    Major Update Add the computer choice as an image Add restart button once game ends Make button creation more dynamic Add delay when player selects choice to show comparison then transitions back to Selecting a choice

--- javascript.js
+++ javascript.js
@@ -1,60 +1,60 @@
 //AUTHOR JESS HAMILTON
 
+//IMPORTING FROM THE HTML
 const game = document.querySelector("#game")
 const resultsDiv = document.querySelector('#results')
-const rps = document.querySelectorAll("#rps button")
-const scoreDiv = document.createElement('div')
+const rps = document.querySelector("#rps")
 
+//CREATING ELEMENTS TO ADD TO THE HTML
+const scoreDiv = document.createElement('div')
+scoreDiv.id='score'
+game.prepend(scoreDiv)
 const playerChoice = document.createElement('p')
 const cpuChoice = document.createElement('p')
 const roundResult = document.createElement('p')
 const playerScoreDisplay = document.createElement('p')
 const cpuScoreDisplay = document.createElement('p')
 
+let playerScore =  0
+let cpuScore = 0
+const choices = ['rock', 'paper', 'scissors']
 
 
-scoreDiv.id='score'
-game.prepend(scoreDiv)
 
-
-function main(){
-
-    let playerScore = 0
-    let cpuScore = 0
-
-
-    rps.forEach((button) => {
-        button.addEventListener('click', () => {
-            if (playerScore < 5 && cpuScore < 5 ){
-                let player = button.id
-                let cpu = getComputerChoice()
-                let result = playRound(cpu, player)
+function createButtons(){
+    choices.forEach(choice => {
+        const button = document.createElement('button')
+        button.id = choice;
+        const img = document.createElement('img')
+        img.src = `./img/${choice}.jpeg`
+        button.appendChild(img)
+        rps.appendChild(button)
         
-                if (result == "Player Wins!"){
-                    playerScore++
-                }else if (result == "Computer Wins!"){
-                    cpuScore++
-                }
-                scoreDiv.innerHTML = ''
-                resultsDiv.innerHTML = ''
-                displayResults(player,cpu,result,playerScore,cpuScore)
-                scoreDiv.innerHTML = ''
-                scoreDiv.innerHTML = (playerScoreDisplay.textContent = 'Player: ' + playerScore +  '<br>CPU: ' + cpuScore)
-            }
-            if (playerScore == 5 || cpuScore == 5){
-                resultsDiv.innerHTML += `<p>Game Over!</p>`
-                if (playerScore == 5 ){
-                    resultsDiv.innerHTML += `<p>You win!</p>`
-                }
-                else {
-                    resultsDiv.innerHTML += `<p>You lost :( </p>`
-                    resultsDiv.innerHTML += `<h1>REFRESH PAGE TO PLAY AGAIN</h1>`
-                }
-            }
-
+        button.addEventListener('click', ()=> {
+            handlePlayerChoice(choice)
         })
-    })
 
+    })
+}
+
+function handlePlayerChoice(player){
+    if (playerScore < 5 && cpuScore < 5){
+        let cpu = getCpuChoice()
+        let result = playRound(cpu,player)
+
+
+        displayChoices(player, cpu)
+        displayResults(player, cpu, result)
+        updateScore(result)
+
+        if (playerScore < 5 && cpuScore < 5){
+            setTimeout(()=>{
+                resetGame()
+            },1250)
+        }else {
+            endGame()
+        }
+    }
 
 }
 
@@ -62,18 +62,10 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-function getComputerChoice(){
+function getCpuChoice(){
+    let choice = getRandomInt(1)
+    return choices[choice]
 
-    let choice = getRandomInt(3)
-    if (choice == 0){
-        return "rock"
-    } 
-    else if (choice == 1){
-        return "paper"
-    }
-    else {
-        return "scissors"
-    }
 }
 
 
@@ -90,23 +82,92 @@ function playRound(computer, player){
         return "Player Wins!"
     }
 
+}
+
+function updateScore(result){
+    if (result === 'Player Wins!'){
+        playerScore++
+    }else if (result === 'Computer Wins!'){
+        cpuScore++
+    }
+
+    scoreDiv.innerHTML = `
+    <p>You: ${playerScore}</p>
+    <p>Cpu: ${cpuScore}</p>
+    `
+}
+
+function resetGame(){
+    rps.innerHTML = ''
+    resultsDiv.innerHTML = ''
+    createButtons()
+
 
 
 }
+function displayChoices(player, cpu){
+    rps.innerHTML = `
+    <div class='choice'>
+        <p>Player</p>
+        <img src='./img/${player}.jpeg' alt='${player}'>
+    </div>
+        <div class='choice'>
+        <p>CPU</p>
+        <img src='./img/${cpu}.jpeg' alt='${cpu}'>
+    </div>
+    `
+}
 
-function displayResults(player, cpu, result, playerScore, cpuScore){
+function displayResults(player, cpu, result){
 
-    playerChoice.textContent = `Player: ${player}`
-    cpuChoice.textContent = `CPU: ${cpu}`
+    playerChoice.textContent =  `You: ${player}`
+    cpuChoice.textContent = `Cpu: ${cpu}`
     roundResult.textContent = `Results: ${result}`
-
 
     resultsDiv.appendChild(playerChoice)
     resultsDiv.appendChild(cpuChoice)
     resultsDiv.appendChild(roundResult)
-
-
-    return
 }
+
+function endGame(){
+    resultsDiv.innerHTML = ''
+    resultsDiv.innerHTML += `<p>Game Over</p>`
+    if (playerScore == 5){
+        resultsDiv.innerHTML += `<p>You Win!</p>`
+    }else {
+        resultsDiv.innerHTML += `<p>You lost :(`
+    }
+    createRestartButton()
+
+}
+
+function createRestartButton(){
+    const restartButton = document.createElement('button')
+    restartButton.textContent = 'Restart Game'
+    restartButton.id = 'restartButton'
+    resultsDiv.appendChild(restartButton)
+    restartButton.addEventListener('click',restartGame)
+    
+}
+
+function restartGame(){
+    playerScore = 0
+    cpuScore = 0
+    resultsDiv.innerHTML = ''
+    scoreDiv.innerHTML = 
+    `
+    <p>You: ${playerScore}</p>
+    <p>CPU: ${cpuScore}</p>
+    `
+    rps.innerHTML = ''
+    createButtons()
+}
+
+
+
+function main(){
+    createButtons()
+}
+
 
 main()
